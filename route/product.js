7e0cae3b9f73e0958c33bca847431897fa9ad5c3
Sougata Bauri <90753996+RahulIsBestCoder@users.noreply.github.com>
2023-03-23 17:50:35 +0530

    added default route
    router.get("/",controllerProduct.Default)

--- route/product.js
+++ route/product.js
@@ -2,6 +2,7 @@ const controllerProduct =require("../controller/product")
 const express=require("express")
 const router = express.Router()  
 
+router.get("/",controllerProduct.Default)
 router.get("/prod",controllerProduct.Data)
 router.get("/prodbyid",controllerProduct.DataId)
 router.post("/prod",controllerProduct.dataPost)
